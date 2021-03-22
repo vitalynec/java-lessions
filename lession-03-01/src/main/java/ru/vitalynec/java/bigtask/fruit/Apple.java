bc0b14fef0f902b53b98b8d50a741f6ca5cf3c12
vitalynec <vitaly.nec@gmail.com>
2021-03-22 18:51:20 +0300

    homework #1 done

---
+++ lession-03-01/src/main/java/ru/vitalynec/java/bigtask/fruit/Apple.java
@@ -0,0 +1,11 @@
+package ru.vitalynec.java.bigtask.fruit;
+
+public class Apple extends Fruit {
+
+    static float weight = 1.0f;
+
+    @Override
+    public float getWeight() {
+        return weight;
+    }
+}
---
+++ lession-03-01/src/main/java/ru/vitalynec/java/bigtask/fruit/Orange.java
@@ -0,0 +1,13 @@
+package ru.vitalynec.java.bigtask.fruit;
+
+import ru.vitalynec.java.bigtask.fruit.Fruit;
+
+public class Orange extends Fruit {
+
+    static float weight = 1.5f;
+
+    @Override
+    public float getWeight() {
+        return weight;
+    }
+}
